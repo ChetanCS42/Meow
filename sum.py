7ddd92abbf97cdfc0d746726895aaed568cf2b81
ChetanCS42 <158749056+ChetanCS42@users.noreply.github.com>
2025-01-23 19:41:44 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,11 @@
+# Sum of two numbers
+
+a = 5
+b = 6
+sum = a + b
+print(sum)
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+s = a+b
+print("The Sum of two number's is: ",s)
